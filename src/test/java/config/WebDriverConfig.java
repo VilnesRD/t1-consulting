36e7f9f4ccf814eb738f5442c82f17df4703e519
Dmitry Rodichev <rdp9000@gmail.com>
2023-01-28 14:05:22 +0700

    Added @Owner

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -7,7 +7,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:./properties/${envBrowser}-browser.properties"
+        "classpath:./properties/${env}"
 })
 
 public interface WebDriverConfig extends Config {
