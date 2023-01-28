a87339539fd790921d4a4f167670df90faca0058
Dmitry Rodichev <rdp9000@gmail.com>
2023-01-28 13:57:00 +0700

    Added @Owner

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -1,30 +1,38 @@
 package config;
 
+
 import org.aeonbits.owner.Config;
 
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "classpath:${resources}.properties"
+        "system:properties",
+        "classpath:./properties/${envBrowser}-browser.properties"
 })
 
 public interface WebDriverConfig extends Config {
 
     @Key("browser")
+    @DefaultValue("chrome")
     String getBrowser();
 
     @Key("baseUrl")
+    @DefaultValue("https://www.t1-consulting.ru")
     String getBaseUrl();
 
     @Key("browserVersion")
+    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("browserSize")
+    @DefaultValue("1920x1080")
     String getBrowserSize();
 
     @Key("ifRemote")
+    @DefaultValue("false")
     Boolean IfRemote();
 
     @Key("remote")
     String getRemote();
-
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import config.WebDriverProvider;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebDriverConfig;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,12 +10,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.aeonbits.owner.ConfigFactory;
 
+import config.WebDriverProvider;
+import config.WebDriverConfig;
+import helpers.Attach;
+
 
 
 public class TestBase {
 
     private static WebDriverConfig config;
     private static WebDriverProvider configuration;
+
     @BeforeAll
     static void setUp() {
 
