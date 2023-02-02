9cd6a9f53ddf7085a5a8b0a43b5c57acbae52293
Dmitry Rodichev <rdp9000@gmail.com>
2023-02-02 21:10:11 +0700

    Update WebDriverConfig

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -5,33 +5,27 @@ import org.aeonbits.owner.Config;
 
 
 @Config.Sources({
-        "classpath:./resources/properties/${env}.properties"
+        "classpath:properties/${env}.properties"
 })
 
 public interface WebDriverConfig extends Config {
 
     @Key("browser")
-    @DefaultValue("chrome")
     String getBrowser();
 
     @Key("baseUrl")
-    @DefaultValue("https://www.t1-consulting.ru")
     String getBaseUrl();
 
     @Key("browserVersion")
-    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("browserSize")
-    @DefaultValue("1920x1080")
     String getBrowserSize();
 
     @Key("isRemote")
-    @DefaultValue("true")
     Boolean IsRemote();
 
     @Key("remote")
-    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String getRemote();
 
 }
